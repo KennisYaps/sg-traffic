dd5b0fb110e167336235e339575a34717a23eedf
yappps <yapyuanxin@gmail.com>
2018-05-05 19:44:43 +0800

    added getData() in Main to fetch trafficIncident

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Main.css";
-import SideBarAndMap from "../SideBarAndMap/SideBarAndMap"
+import SideBarAndMap from "../SideBarAndMap/SideBarAndMap";
+import { getData } from "../../utils/fetchAPI";
+class Main extends Component {
+  constructor() {
+    super();
+    this.state = {
+      trafficIncidentsDatas: []
+    };
+  }
+  render() {
+    return (
+      <div id="Main">
+        <SideBarAndMap incidentsDatas={this.state.trafficIncidentsDatas} />
+      </div>
+    );
+  }
+  async componentDidMount() {
+    const incidentsDatas = await getData("TrafficIncidents");
+    this.setState({ trafficIncidentsDatas: incidentsDatas });
+    // this.interval = await setInterval(data, 1000); // 2 mins
+  }
 
-const Main = () => (
-  <div id="Main">
-    <SideBarAndMap />
-  </div>
-);
+  componentWillUnmount() {
+    // clearInterval(this.interval);
+  }
+}
 
 export default Main;
