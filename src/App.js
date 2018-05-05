6f50783f4c3d4cc30e7d359e9c10c4b31bda58f2
yappps <yapyuanxin@gmail.com>
2018-05-05 15:29:31 +0800

    added Main component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
-
+import Main from "./Components/Main/Main";
 class App extends Component {
   render() {
-    return <div className="App">Hello</div>;
+    return (
+      <div className="App">
+        <h1>Welcome to My Traffic App</h1>
+        <Main />
+      </div>
+    );
   }
 }
 
---
+++ src/Components/Main/Main.js
@@ -0,0 +1,6 @@
+import React from "react";
+import './Main.css';
+
+const Main = () => <div id="Main">This is a Main component</div>;
+
+export default Main;
