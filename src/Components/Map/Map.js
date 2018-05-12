1541e0a3baf2b1690dfb3c4d248c41897d48ea43
yappps <yapyuanxin@gmail.com>
2018-05-12 15:11:03 +0800

    edited map

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,57 +1,11 @@
 import React from "react";
-const { compose, withProps, withHandlers } = require("recompose");
-const {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker,
-  TrafficLayer
-} = require("react-google-maps");
-const {
-  MarkerClusterer
-} = require("react-google-maps/lib/components/addons/MarkerClusterer");
-const Map = compose(
-  withProps({
-    googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places",
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `600px` }} />,
-    mapElement: <div style={{ height: `100%` }} />
-  }),
-  withHandlers({
-    onMarkerClustererClick: () => markerClusterer => {
-      const clickedMarkers = markerClusterer.getMarkers();
-      console.log(`Current clicked markers length: ${clickedMarkers.length}`);
-      console.log(clickedMarkers);
-    }
-  }),
-  withScriptjs,
-  withGoogleMap
-)(props => (
-  <GoogleMap
-    defaultZoom={12}
-    defaultCenter={{
-      lat: 1.29027,
-      lng: 103.851959
-    }}
-  >
-    <MarkerClusterer
-      onClick={props.onMarkerClustererClick}
-      averageCenter
-      enableRetinaIcons
-      gridSize={60}
-    >
-      {props.markers.map((marker, idx) => (
-        <Marker
-          key={idx}
-          position={{ lat: marker.Latitude, lng: marker.Longitude }}
-          onMouseOver={() => {
-            console.log(marker.Message)
-          }}
-        />
-      ))}
-    </MarkerClusterer>
-    <TrafficLayer autoUpdate />
-  </GoogleMap>
-));
+import "../Map/Map.css";
+
+const Map = props => (
+  <div id="Map">
+    <h1>Map component</h1>
+    {props.results.map((i, idx) => <p key={idx}>{i.Type}</p>)}
+  </div>
+);
+
 export default Map;
