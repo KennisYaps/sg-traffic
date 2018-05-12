7114d12abdb6ddc97f9694caa45b7ee4712335f7
yappps <yapyuanxin@gmail.com>
2018-05-12 23:22:01 +0800

    added Search

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,31 +1,42 @@
 import React, { Component } from "react";
+import Map from "../Map/Map";
 import "./Main.css";
 import { getData } from "../../utils/fetchAPI";
-import SearchBar from '../SearchBar/SearchBar';
+import Search from "../Search/Search";
 class Main extends Component {
   constructor() {
     super();
     this.state = {
-      trafficIncidentsDatas: []
+      trafficIncidentsDatas: [],
+      matchedResults: []
     };
   }
   render() {
     return (
       <div id="Main">
-        <h4>Current No Of Incidents: {this.state.trafficIncidentsDatas.length}</h4>
-        <SearchBar incidentsDatas={this.state.trafficIncidentsDatas} />
+        <h1>Main Component</h1>
+        <h4>
+          Current Traffic Incident: {this.state.trafficIncidentsDatas.length}{" "}
+        </h4>
+        <Search
+          data={this.state.trafficIncidentsDatas}
+          onSetLocation={this.handleSetLocation}
+          results={this.state.matchedResults}
+        />
+        <Map results={this.state.matchedResults} />
       </div>
     );
   }
   async componentDidMount() {
     const incidentsDatas = await getData("TrafficIncidents");
-    this.setState({ trafficIncidentsDatas: incidentsDatas });
-    // this.interval = await setInterval(data, 1000); // 2 mins
+    this.setState({
+      trafficIncidentsDatas: incidentsDatas,
+      matchedResults: incidentsDatas
+    });
   }
-
-  componentWillUnmount() {
-    // clearInterval(this.interval);
-  }
+  handleSetLocation = location => {
+    this.setState({ matchedResults: location });
+  };
 }
 
 export default Main;
