305d089b8aff8a82dca4733d2cbfc34e0f803fc8
yappps <yapyuanxin@gmail.com>
2018-05-12 15:10:19 +0800

    added landing page content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
-import Main from "./Components/Main/Main";
+import Main from './Components/Main/Main';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Welcome to My Traffic App</h1>
-        <br />
-        <Main />
+        <div id="title">
+          <h1 id="title-h1">What's Happening ?</h1>
+          <p>Displaying Singapore's traffic condition in real time</p>
+        </div>
+        <Main/>
       </div>
     );
   }
