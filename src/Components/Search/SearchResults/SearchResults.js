16d61a7fc5bf33266bf0e8dddeaf67cef4821253
yappps <yapyuanxin@gmail.com>
2018-05-12 20:57:30 +0800

    Added searchResults

---
+++ src/Components/Search/SearchResults/SearchResults.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./SearchResults.css";
+const SearchResults = () => (
+  <div id="SearchResults">
+    <h1>Search Results component</h1>
+  </div>
+);
+
+export default SearchResults;
