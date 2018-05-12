658be913ecf7b9a363e026d20b1682d208a21836
yappps <yapyuanxin@gmail.com>
2018-05-12 23:23:07 +0800

    to display traffic type and message

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -4,7 +4,11 @@ import "../Map/Map.css";
 const Map = props => (
   <div id="Map">
     <h1>Map component</h1>
-    {props.results.map((i, idx) => <p key={idx}>{i.Type}</p>)}
+    {props.results.map((result, idx) => (
+      <p key={idx}>
+        {result.Type}&nbsp;&nbsp;&nbsp;{result.Message}
+      </p>
+    ))}
   </div>
 );
 
--- src/Components/Search/SearchResults/SearchResults.js
+++ src/Components/Search/SearchResults/SearchResults.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./SearchResults.css";
-const SearchResults = () => (
+const SearchResults = props => (
   <div id="SearchResults">
     <h1>Search Results component</h1>
+    {props.results.map((result, idx) => (
+      <p key={idx}>
+        {result.Type}&nbsp;&nbsp;&nbsp;{result.Message}
+      </p>
+    ))}
   </div>
 );
 
